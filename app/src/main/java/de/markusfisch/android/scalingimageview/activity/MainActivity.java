57f8c583b53a9060302034bee84bcaed969c819b
Markus Fisch <mf@markusfisch.de>
2017-02-04 15:07:56 +0100

    Double tap to zoom/restore an image
    Also renamed some symbols for ease of understanding.
    
    And use one Point object instead of two Float's to
    cache touch positions.

--- app/src/main/java/de/markusfisch/android/scalingimageview/activity/MainActivity.java
+++ app/src/main/java/de/markusfisch/android/scalingimageview/activity/MainActivity.java
@@ -2,8 +2,8 @@ package de.markusfisch.android.scalingimageview.activity;
 
 import de.markusfisch.android.scalingimageview.R;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 	@Override
